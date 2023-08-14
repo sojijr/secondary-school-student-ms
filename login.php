8c9531c6ff966516529d31f69fccc24d03aaaa24
Damilola Soji-Oderinde <dammysoji11@gmail.com>
2023-08-14 23:44:33 +0100

    prototype login page

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>Login Page</title>
+</head>
+
+<body>
+    <div class="main-container">
+        <div class="login-container">
+            <h1>Login</h1>
+            <form class="login-form">
+                <input type="text" id="username" class="form-control custom-input" placeholder="Username" required>
+                <input type="password" id="password" class="form-control custom-input" placeholder="Password" required>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+        <div class="school-box">
+            <p>Ruby School</p>
+        </div>
+    </div>
+</body>
+
+</html>
