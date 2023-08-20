d5aff105ad0268798ba97d74169ecf65c8e1eecd
Damilola Soji-Oderinde <dammysoji11@gmail.com>
2023-08-20 22:19:41 +0100

    added header to student profile page

---
+++ model/student/studentProfile.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/images/favicon.png" rel="icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="../../assets/css/styles.css">
+    <link rel="stylesheet" href="../../assets/css/profile.css">
+    <title>Student Profile</title>
+</head>
+
+<body>
+    <header class="profile-header">
+        <img src="../../assets/images/logo.png" alt="School Logo" class="logo">
+        <div class="student-info">
+            <span class="student-name">John Doe</span>
+            <div class="dropdown">
+                <button class="dropbtn">
+                    <img src="../../assets/images/demo-image.jpg" alt="Demo Image" class="demo-image">
+                    <i class="fas fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#"><i class="fas fa-key"></i> Change Password</a>
+                    <a href="#"><i class="fas fa-sign-out-alt"></i> Log Out</a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Profile content goes here -->
+
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
